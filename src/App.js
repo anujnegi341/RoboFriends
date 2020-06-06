4846fe6d561e15d42e8309ed7c0ab19c50a29e89
Anuj Negi <negi.anuj341@gmail.com>
2020-06-06 11:07:45 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './SearchBox'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
+
+  render(){
+    const filteredRobots = this.state.robots.filter(
+      robot => {return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+      }
+    )
+    return (
+      <div className='tc'>
+        <h1>Robo Friends</h1>
+        <SearchBox SearchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
+      </div>
+      );
+  }
 }
+
+
 
 export default App;
